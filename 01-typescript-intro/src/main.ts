4df648504b78ae1952cdea3538d576cc81fcdfc8
Alejandro González González <agongon@arte-consultores.com>
2025-02-03 09:47:41 +0000

    Objetos, arreglos e interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,6 +1,8 @@
 import './style.css';
 
-import './topics/01-basic-types';
+//import './topics/01-basic-types';
+import './topics/02-object-interface';
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo
