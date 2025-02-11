66a602af2bbdcb7c1c847bcfc76f051a61350403
Alejandro González González <agongon@arte-consultores.com>
2025-02-11 22:56:25 +0000

    Servicios

--- 02-Bases/src/app/dbz/pages/main-page.component.ts
+++ 02-Bases/src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Character } from "../interfaces/character.interface";
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,29 +9,9 @@ import { Character } from "../interfaces/character.interface";
 })
 export class MainPageComponent {
 
-  public characters: Character[] = [
-    {
-      name: 'Krilin',
-      power: 1000
-    },
-    {
-      name: 'Goku',
-      power: 9500
-    },
-    {
-      name: 'Vegeta',
-      power: 7500
-    }
-  ];
+  constructor(
+    public dbzService: DbzService
+  ) {}
 
-  onNewCharacter(character: Character):void {
-    console.log(character);
-    this.characters.push(character);
-  }
 
-  onDeleteCharacter(index:number): void {
-    console.log(index);
-    this.characters.splice(index,1);
-    console.log(this.characters);
-  }
 }
