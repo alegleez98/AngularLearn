f3acbbe083969567a5dead70618416e1810607f7
Alejandro González González <agongon@arte-consultores.com>
2025-02-25 23:16:28 +0000

    Reforzar lo aprendido hasta ahora

--- 02-Bases/src/app/pages/counter/counter-page.component.ts
+++ 02-Bases/src/app/pages/counter/counter-page.component.ts
@@ -1,26 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter-page.component.html',
-  styleUrl: './counter-page.component.css'
+  styleUrl: './counter-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterPageComponent {
 
   counter = 10;
   counterSignal = signal(10);
 
+  constructor() { }
+
   increasyBy( value: number) {
     this.counter += value;
     this.counterSignal.update((current) => current + value);
-
   }
 
   reset() {
     this.counter = 0;
     this.counterSignal.set(0);
-
   }
 
 }
