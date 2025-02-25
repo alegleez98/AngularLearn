d2d9e805879f761c9692e5e91616378642ff032e
Alejandro González González <agongon@arte-consultores.com>
2025-02-25 23:29:22 +0000

    Señales computadas Readonly signals

--- 02-Bases/src/app/pages/hero/hero-page.component.ts
+++ 02-Bases/src/app/pages/hero/hero-page.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [ CommonModule ],
+  imports: [CommonModule],
   templateUrl: './hero-page.component.html',
   styleUrl: './hero-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,9 +13,12 @@ export class HeroPageComponent {
   public name = signal('Ironman');
   public age = signal(45);
 
-  getHeroDescription() {
-    return `${this.name()} - ${this.age()}`;
-  }
+  heroDescription = computed(() => {
+    const description = `${this.name()} - ${this.age()}`;
+    return description;
+  })
+
+  capitalizedName = computed(() => this.name().toUpperCase());
 
   changeHero() {
     this.name.set('Spiderman');
