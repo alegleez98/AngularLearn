63880cae1ea751ecd0e49387b9c0c6c399b8a065
Alejandro González González <agongon@arte-consultores.com>
2025-02-26 16:39:50 +0000

    @for-Realizar iteraciones de elementos

--- 02-Bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-Bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+
+interface Character {
+  id: number,
+  name: string,
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball',
@@ -7,4 +13,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './dragonball-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DragonballPageComponent { }
+export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    {id:1, name: 'Goku', power: 9001},
+    {id:1, name: 'Vegetta', power: 8000},
+    {id:1, name: 'Krillin', power: 3000}
+  ]);
+}
