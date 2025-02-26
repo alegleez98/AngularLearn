c8ee9236b9a37d522b49ad131091210cd45e629a
Alejandro González González <agongon@arte-consultores.com>
2025-02-26 16:30:58 +0000

    Tarea - diseño página

---
+++ 02-Bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball',
+  imports: [],
+  templateUrl: './dragonball-page.component.html',
+  styleUrl: './dragonball-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DragonballPageComponent { }
