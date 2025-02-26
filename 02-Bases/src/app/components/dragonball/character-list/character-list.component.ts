7fcefee98ddc94080829ac519ffcb4d616081298
Alejandro González González <agongon@arte-consultores.com>
2025-02-26 23:43:35 +0000

    Tarea- signal inputs y creación de componentes

--- 02-Bases/src/app/components/dragonball/character-list/character-list.component.ts
+++ 02-Bases/src/app/components/dragonball/character-list/character-list.component.ts
@@ -10,6 +10,7 @@ import type { Character } from '../../../interfaces/character.interface';
 export class CharacterListComponent {
 
   characters = input.required<Character[]>();
+  listName = input.required<String>();
 
 
 
--- 02-Bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-Bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -2,11 +2,13 @@
 import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
 import { Character } from '../../interfaces/character.interface';
+import { CharacterAddComponent } from "../../components/dragonball/character-add/character-add.component";
 
 @Component({
   selector: 'app-dragonball-super',
   imports: [
-    CharacterListComponent
+    CharacterListComponent,
+    CharacterAddComponent
 ],
   templateUrl: './dragonball-super-page.component.html',
   styleUrl: './dragonball-super-page.component.css',
@@ -14,34 +16,12 @@ import { Character } from '../../interfaces/character.interface';
 })
 export class DragonballSuperPageComponent {
 
-  name = signal('');
-  power = signal(0);
-
   characters = signal<Character[]>([
     {id:1, name: 'Goku', power: 9001},
     {id:2, name: 'Vegetta', power: 8000},
     // {id:3, name: 'Piccolo', power: 3000},
     // {id:4, name: 'Yamcha', power: 500}
   ]);
-
-  addCharacter() {
-    if( !this.name() || !this.power() || this.power() <= 0) {
-      return;
-    }
-    const newCharacter: Character = {
-      id: this.characters().length + 1,
-      name: this.name(),
-      power: this.power(),
-    }
-
-    this.characters.update( (list) => [...list, newCharacter]);
-    this.resetFields();
-  }
-
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
-  }
 
   // powerClasses = computed(() => {
   //   return {
