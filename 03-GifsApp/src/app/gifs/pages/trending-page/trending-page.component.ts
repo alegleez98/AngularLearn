5cd3e885035790c38e5f0f82421e5e1982ddc423
Alejandro González González <agongon@arte-consultores.com>
2025-02-28 19:20:52 +0000

    Mostrar imagenes de relleno

--- 03-GifsApp/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 03-GifsApp/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifsListComponent } from "../../components/gifs-list/gifs-list.component";
 
 @Component({
   selector: 'app-trending',
-  imports: [],
+  imports: [GifsListComponent],
   templateUrl: './trending-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
