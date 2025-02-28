133d8b73c70bcf8666d8501ac77bdc31424e69ca
Alejandro González González <agongon@arte-consultores.com>
2025-02-28 17:53:37 +0000

    Componentes para el menú lateral

--- 03-GifsApp/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ 03-GifsApp/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GifsSideMenuComponent } from "../../components/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GifsSideMenuComponent],
   templateUrl: './dashboard-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
