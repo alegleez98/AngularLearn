dd5de8ef616aa05b03265d81f95f357b3eafc69c
Alejandro González González <agongon@arte-consultores.com>
2025-03-01 10:14:38 +0000

    Mostrar Gifs en pantalla

--- 03-GifsApp/src/app/gifs/services/Gifs.service.ts
+++ 03-GifsApp/src/app/gifs/services/Gifs.service.ts
@@ -13,6 +13,7 @@ export class GifsService {
   private http = inject(HttpClient);
 
   trendingGifs = signal<Gif[]>([]);
+  trendingGifsLoading = signal(true);
 
   constructor() {
     this.loadTrendingGifs();
@@ -28,6 +29,7 @@ export class GifsService {
     }).subscribe( (resp) => {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
       this.trendingGifs.set(gifs);
+      this.trendingGifsLoading.set(false);
     });
   }
 
