549e2593e837aba4851ddd95c985d7b22184dd4e
Alejandro González González <agongon@arte-consultores.com>
2025-03-03 18:42:55 +0000

    Diseño Masonry

--- 03-GifsApp/src/app/gifs/services/Gifs.service.ts
+++ 03-GifsApp/src/app/gifs/services/Gifs.service.ts
@@ -31,6 +31,15 @@ export class GifsService {
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
+  trendingGifGroup = computed<Gif[][]>( () => {
+    const groups = [];
+    for ( let i = 0; i<this.trendingGifs().length; i +=3) {
+      groups.push( this.trendingGifs().slice(i, i+3));
+    }
+    console.log(groups);
+    return groups;
+  })
+
   constructor() {
     this.loadTrendingGifs();
   }
