5acd21a314be8709793552d0164c2c4c59e31417
Alejandro González González <agongon@arte-consultores.com>
2025-03-03 19:27:07 +0000

    Preservar posición del scroll

--- 03-GifsApp/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 03-GifsApp/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,19 +1,27 @@
 import { ChangeDetectionStrategy, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
-import { GifsListComponent } from "../../components/gifs-list/gifs-list.component";
 import { GifsService } from '../../services/Gifs.service';
+import { ScrollStateService } from '../../../shared/services/scroll-state.service';
 
 
 
 @Component({
   selector: 'app-trending',
-  imports: [GifsListComponent],
   templateUrl: './trending-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class TrendingPageComponent {
   gifService = inject( GifsService );
+  scrollStateService = inject( ScrollStateService );
 
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if ( !scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+
+  }
 
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
@@ -22,7 +30,7 @@ export default class TrendingPageComponent {
     const clientHeight:number = scrollDiv.clientHeight;
     const scrollHeight: number = scrollDiv.scrollHeight;
     const isAtBottom: boolean = scrollTop + clientHeight + 300 >= scrollHeight;
-    console.log(isAtBottom);
+    this.scrollStateService.trendingScrollState.set(scrollTop);
     if (isAtBottom) {
       this.gifService.loadTrendingGifs();
     }
