972d0291ea7a978b4fdf0b89cd4151c932f84198
Alejandro González González <agongon@arte-consultores.com>
2025-03-04 16:46:28 +0000

    Reactividad con resources

--- 04-Country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 04-Country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/searchInput/searchInput.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
-import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -13,25 +13,13 @@ export class ByCapitalPageComponent {
 
   countryService = inject(CountryService);
 
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
+  query = signal('');
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async({ request }) => {
+      if ( !request.query ) return [];
+      return await firstValueFrom(this.countryService.searchByCapital(request.query));
+    }
+  })
 
-  onSearch(query: string): void {
-    if(this.isLoading()) return;
-    this.isLoading.set(true);
-    this.isError.set(null);
-    this.countryService.searchByCapital(query).subscribe({
-      next: (countries) => {
-        this.isLoading.set(false);
-        this.countries.set(countries);
-        console.log(countries);
-      },
-      error: (err) => {
-        this.isLoading.set(false);
-        this.countries.set([]);
-        this.isError.set(err);
-      }
-    });
-  }
 }
