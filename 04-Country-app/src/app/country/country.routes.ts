f6ad9438374e299965491c6add1b94d2f7b70e7e
Alejandro González González <agongon@arte-consultores.com>
2025-03-04 09:01:10 +0000

    Layout components

--- 04-Country-app/src/app/country/country.routes.ts
+++ 04-Country-app/src/app/country/country.routes.ts
@@ -1,11 +1,23 @@
-import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
+import { CountryLayoutComponent } from "./layouts/CountryLayout/CountryLayout.component";
 import { Routes } from "@angular/router";
+import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
 
 
 const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent,
+      },
+
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      }
+    ]
   }
 ];
 
