3bb3f24e17677441bc8a762582cdc52576c90c37
Alejandro González González <agongon@arte-consultores.com>
2025-03-04 17:16:47 +0000

    Mostrar diferentes estados al usuario

--- 04-Country-app/src/app/country/services/country.service.ts
+++ 04-Country-app/src/app/country/services/country.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { CountryMapper } from '../mapper/country.mapper';
-import { map, Observable, catchError, throwError } from 'rxjs';
+import { map, Observable, catchError, throwError, delay } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 const API_URL = 'https://restcountries.com/v3.1';
@@ -32,6 +32,7 @@ export class CountryService {
     return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`)
       .pipe(
         map((restCountries) => CountryMapper.mapRestCountryArrayToCountryArray(restCountries)),
+        delay(3000),
         catchError((error) => {
           console.log('Error fetching ', error);
           return throwError(() => new Error(`No se pudieron obtener paises con ese query: ${query}`));
