dc840aa927de2b4ddba4c58e8dc2c8fd76adf87e
Alejandro González González <agongon@arte-consultores.com>
2025-03-04 11:47:50 +0000

    CountryService - Interfaces

--- 04-Country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 04-Country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchInputComponent } from "../../components/searchInput/searchInput.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,8 +10,11 @@ import { CountryListComponent } from "../../components/country-list/country-list
 })
 export class ByCapitalPageComponent {
 
+  countryService = inject(CountryService);
 
-  onSearch(value: string): void {
-    console.log(value);
+  onSearch(query: string): void {
+    this.countryService.searchByCapital(query).subscribe(paises => {
+      console.log(paises);
+    });
   }
 }
