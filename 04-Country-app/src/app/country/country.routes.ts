4951eb8fc739d905c016d69ea105ae043bc22cd4
Alejandro González González <agongon@arte-consultores.com>
2025-03-04 10:24:24 +0000

    Inputs y Tablas + Tarea

--- 04-Country-app/src/app/country/country.routes.ts
+++ 04-Country-app/src/app/country/country.routes.ts
@@ -1,6 +1,9 @@
 import { CountryLayoutComponent } from "./layouts/CountryLayout/CountryLayout.component";
 import { Routes } from "@angular/router";
 import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
+import { ByRegionPageComponent } from "./pages/by-region-page/by-region-page.component";
+import { ByCountryPageComponent } from "./pages/by-country-page/by-country-page.component";
+import { CountryPageComponent } from "./pages/countryPage/countryPage.component";
 
 
 const countryRoutes: Routes = [
@@ -11,6 +14,18 @@ const countryRoutes: Routes = [
       {
         path: 'by-capital',
         component: ByCapitalPageComponent,
+      },
+      {
+        path: 'by-country',
+        component: ByCountryPageComponent,
+      },
+      {
+        path: 'by-region',
+        component: ByRegionPageComponent,
+      },
+      {
+        path: 'by/:codeCountry',
+        component: CountryPageComponent,
       },
 
       {
--- 04-Country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 04-Country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { SearchInputComponent } from "../../components/searchInput/searchInput.component";
+import { CountryListComponent } from "../../components/country-list/country-list.component";
 
 @Component({
   selector: 'app-by-capital-page',
-  imports: [],
+  imports: [SearchInputComponent, CountryListComponent],
   templateUrl: './by-capital-page.component.html',
 })
-export class ByCapitalPageComponent { }
+export class ByCapitalPageComponent {
+
+
+  onSearch(value: string): void {
+    console.log(value);
+  }
+}
