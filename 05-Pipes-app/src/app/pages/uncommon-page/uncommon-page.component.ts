cf9bebfc356aa7132a8609befa81674724ab5f82
Alejandro González González <agongon@arte-consultores.com>
2025-03-26 20:13:31 +0000

    i18nSelectPipe

--- 05-Pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 05-Pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Alejandro',
+  gender: 'male',
+  age: 26,
+  address: 'Tenerife, España'
+}
+
+const client2 = {
+  name: 'Laura',
+  gender: 'female',
+  age: 27,
+  address: 'Tenerife, España'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  //i18nSelect
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient() {
+    if ( this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+    return;
+  }
+
+
+}
