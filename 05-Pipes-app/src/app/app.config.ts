27fdb1ed5c3216a4cbdaa57e7f5d57bc11bb0d6e
Alejandro González González <agongon@arte-consultores.com>
2025-03-26 19:08:43 +0000

    Cambiar de idiomas

--- 05-Pipes-app/src/app/app.config.ts
+++ 05-Pipes-app/src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import localeEs from '@angular/common/locales/es'
+import localeFr from '@angular/common/locales/fr'
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeFr, 'fr');
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+
+
+  ]
 };
