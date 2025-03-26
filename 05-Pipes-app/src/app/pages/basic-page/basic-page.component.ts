9d25085dfa4810ed8acbd62e66c743cd0f059009
Alejandro González González <agongon@arte-consultores.com>
2025-03-26 18:56:22 +0000

    Date pipe

--- 05-Pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ 05-Pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
@@ -11,6 +11,19 @@ export default class BasicPageComponent {
   nameLower = signal('alejandro');
   nameUpper = signal('ALEJANDRO');
   fullName = signal('AleJanDrO GonzAleZ');
+
+  customDate = signal( new Date() );
+
+  tickingDateEffect = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+
+    onCleanUp(() => {
+      clearInterval(interval);
+    })
+  })
 
 
 }
