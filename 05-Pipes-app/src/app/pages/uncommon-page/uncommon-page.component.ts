d3e3f81070c8c7e69fbc406aaa90d3a793f0875b
Alejandro González González <agongon@arte-consultores.com>
2025-03-26 20:23:49 +0000

    i18nPluralPipe

--- 05-Pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 05-Pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 const client1 = {
   name: 'Alejandro',
@@ -18,12 +18,12 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
 
-  //i18nSelect
+  //i18n Select
   client = signal(client1);
 
   invitationMap = {
@@ -41,5 +41,26 @@ export default class UncommonPageComponent {
     return;
   }
 
+  //i18n Plural
+
+  clientsMap = signal({
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    other: 'tenemos # clientes esperando'
+  });
+
+  clients = signal([
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Alejandro',
+    'Laura',
+    'Juan'
+  ]);
+
+  deleteClient() {
+    this.clients.update( prev => prev.slice(1));
+  }
 
 }
