f427e56b2a7ffd9cda8f607f07b2bc061152f3c8
Alejandro González González <agongon@arte-consultores.com>
2025-04-08 18:29:43 +0100

    CanFlyPipe y HeroColor

--- 05-Pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 05-Pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { ColorHeroPipe } from '../../pipes/color-hero.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ ToggleCasePipe],
+  imports: [ ToggleCasePipe, CanFlyPipe, ColorHeroPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
