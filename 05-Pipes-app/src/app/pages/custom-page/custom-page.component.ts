23130e81fc5e4e59757d54c3763fd8df86679113
Alejandro González González <agongon@arte-consultores.com>
2025-04-08 18:43:12 +0100

    HeroTextColor

--- 05-Pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 05-Pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -3,10 +3,13 @@ import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { ColorHeroPipe } from '../../pipes/color-hero.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ ToggleCasePipe, CanFlyPipe, ColorHeroPipe],
+  imports: [ ToggleCasePipe, CanFlyPipe, ColorHeroPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
