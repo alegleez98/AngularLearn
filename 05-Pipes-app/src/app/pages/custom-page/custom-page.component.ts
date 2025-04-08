6b5a472349f452e5e223fb2fbfb7c221d88a2f2b
Alejandro González González <agongon@arte-consultores.com>
2025-04-08 19:14:52 +0100

    HeroFilterPipe

--- 05-Pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 05-Pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -8,10 +8,11 @@ import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
 import { Hero } from '../../interfaces/hero.interface';
+import { HeroFilterPipe } from '../../pipes/heroFilter.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ ToggleCasePipe, CanFlyPipe, ColorHeroPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe],
+  imports: [ ToggleCasePipe, CanFlyPipe, ColorHeroPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe, HeroFilterPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
@@ -23,6 +24,8 @@ export default class CustomPageComponent {
   heroes = signal(heroes);
 
   sortBy = signal<keyof Hero | null>(null);
+
+  searchQuery = signal('');
 
   changeUpper() {
     this.upperCase.set(!this.upperCase());
