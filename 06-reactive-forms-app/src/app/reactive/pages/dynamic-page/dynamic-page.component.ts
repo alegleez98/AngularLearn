9c5123bd8c310620e8f2879a095b8b169c8aae44
Alejandro González González <agongon@arte-consultores.com>
2025-04-13 10:30:28 +0100

    rutas padre y template HTML

--- 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,8 +1,10 @@
+import { JsonPipe } from '@angular/common';
+import { JsonpClientBackend } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './dynamic-page.component.html',
 })
 export class DynamicPageComponent { }
