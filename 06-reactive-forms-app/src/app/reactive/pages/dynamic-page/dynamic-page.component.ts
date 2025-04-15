2cb09a43cfaf60a66fc0ac6a153f0c3062d56db3
Alejandro González González <agongon@arte-consultores.com>
2025-04-15 18:58:14 +0100

    Formularios dinamicos con arreglos

--- 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,31 @@
 import { JsonPipe } from '@angular/common';
 import { JsonpClientBackend } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent { }
+export class DynamicPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Mario Bros', Validators.required],
+    ], Validators.minLength(3))
+
+  });
+
+  get favoriteGames(): FormArray {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+
+
+
+}
