81e31ef5d467075430acbc9618696ae68299d584
Alejandro González González <agongon@arte-consultores.com>
2025-04-17 10:57:59 +0100

    Mapa a pantalla completa

--- 08-maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ 08-maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../../../environments/environment';
+
+
+mapboxgl.accessToken = environment.mapboxKey;
+
 
 @Component({
   selector: 'app-fullscreen-map-page',
   imports: [],
   templateUrl: './fullscreen-map-page.component.html',
+  styleUrl: './fullscreen-map-page.component.css',
 })
-export class FullscreenMapPageComponent { }
+export class FullscreenMapPageComponent implements AfterViewInit {
+
+  divElement = viewChild<ElementRef>('map');
+
+  async ngAfterViewInit() {
+    if ( !this.divElement()) return;
+    await new Promise((resolve) => setTimeout(resolve, 80));
+    const element = this.divElement()?.nativeElement;
+    console.log(element);
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-16.605555555556, 28.268611111111], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+
+
+
+
+
+}
