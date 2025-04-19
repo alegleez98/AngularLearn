19f11ddf3419ad0098652a4557b7571f7b38ee56
Alejandro González González <agongon@arte-consultores.com>
2025-04-19 12:24:51 +0100

    pagina de productos por genero

--- 09-tesloShop-app/src/app/store-front/pages/gender-page/gender-page.component.ts
+++ 09-tesloShop-app/src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { ProductService } from '@products//services/Product.service';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCardComponent } from '@products//components/product-card/product-card.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent { }
+export class GenderPageComponent {
+  route = inject(ActivatedRoute);
+  private _productsService = inject(ProductService);
+  public get productsService() {
+    return this._productsService;
+  }
+  public set productsService(value) {
+    this._productsService = value;
+  }
+
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsResource = rxResource({
+    request: () => ({ gender: this.gender() }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        gender: request.gender,
+      });
+    },
+  });
+}
