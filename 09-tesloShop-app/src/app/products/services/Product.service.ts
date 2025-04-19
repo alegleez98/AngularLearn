e590e61bd891982adf2222280d18d60b96c246ab
Alejandro González González <agongon@arte-consultores.com>
2025-04-19 11:01:50 +0100

    listado de productos

--- 09-tesloShop-app/src/app/products/services/Product.service.ts
+++ 09-tesloShop-app/src/app/products/services/Product.service.ts
@@ -2,18 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { ProductsResponse } from '../interfaces/product.interface';
+import { environment } from 'src/environments/environment';
+
+const BASE_URL = environment.baseUrl
+
+interface Options {
+  limit?: number;
+  offset?: number;
+  gender?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private urlBack = 'http://localhost:3000';
 
   private http = inject(HttpClient);
 
-  getProducts(): Observable<ProductsResponse> {
-    return this.http.get<ProductsResponse>(`${this.urlBack}/api/products`).pipe(tap((resp) => console.log(resp)));
+  getProducts(options: Options): Observable<ProductsResponse> {
+    const { limit = 9, offset = 9, gender = '' } = options;
+    console.log(limit);
+    return this.http.get<ProductsResponse>(`${BASE_URL}/products`, {
+      params: {
+        limit: limit,
+        offset: offset,
+        gender: gender,
+      }
+    }).pipe(tap((resp) => console.log(resp)));
   }
 
   constructor() { }
