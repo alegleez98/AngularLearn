cd01004fd1c42b9de80f95eb36a13301fdabb8bd
Alejandro González González <agongon@arte-consultores.com>
2025-04-20 12:21:30 +0100

    Paginacion local

--- 09-tesloShop-app/src/app/store-front/pages/home-page/home-page.component.ts
+++ 09-tesloShop-app/src/app/store-front/pages/home-page/home-page.component.ts
@@ -2,11 +2,11 @@ import { Component, inject, resource } from '@angular/core';
 import { ProductCardComponent } from '@products//components/product-card/product-card.component';
 import { ProductService } from '@products//services/Product.service';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { ProductsResponse } from '@products//interfaces/product.interface';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
