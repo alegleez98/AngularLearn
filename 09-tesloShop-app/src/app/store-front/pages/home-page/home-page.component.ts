4d4730652a82afb9b520bad8768f982d18e61003
Alejandro González González <agongon@arte-consultores.com>
2025-04-20 12:43:14 +0100

    Paginacion con RxResource

--- 09-tesloShop-app/src/app/store-front/pages/home-page/home-page.component.ts
+++ 09-tesloShop-app/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, inject, resource } from '@angular/core';
 import { ProductCardComponent } from '@products//components/product-card/product-card.component';
 import { ProductService } from '@products//services/Product.service';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -12,11 +14,21 @@ import { PaginationComponent } from '@shared/components/pagination/pagination.co
 export class HomePageComponent {
 
   private productsService = inject(ProductService);
+  private activatedRoute =  inject(ActivatedRoute);
+
+  currentPage = toSignal( this.activatedRoute.queryParamMap.pipe(
+    map( params => (params.get('page') ? +params.get('page')! : 1)),
+    map( page => (isNaN(page) ? 1 : page))
+  ), {
+    initialValue: 1
+  })
 
   productsResource = rxResource( {
-    request: () => ({}),
+    request: () => ({page: this.currentPage() - 1}),
     loader: ({request}) => {
-      return this.productsService.getProducts({});
+      return this.productsService.getProducts({
+        offset: request.page * 9
+      });
     }
   });
 
