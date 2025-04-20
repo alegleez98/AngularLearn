48677129cdd5501c1b394a040a9d028fe2cc5506
Alejandro González González <agongon@arte-consultores.com>
2025-04-20 12:52:15 +0100

    Servicio de paginacion

--- 09-tesloShop-app/src/app/store-front/pages/gender-page/gender-page.component.ts
+++ 09-tesloShop-app/src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -4,14 +4,17 @@ import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { ProductCardComponent } from '@products//components/product-card/product-card.component';
 import { map } from 'rxjs';
+import { PaginationComponent } from "../../../shared/components/pagination/pagination.component";
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './gender-page.component.html',
 })
 export class GenderPageComponent {
   route = inject(ActivatedRoute);
+  paginationService = inject(PaginationService);
   private _productsService = inject(ProductService);
   public get productsService() {
     return this._productsService;
@@ -23,10 +26,10 @@ export class GenderPageComponent {
   gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
 
   productsResource = rxResource({
-    request: () => ({ gender: this.gender() }),
+    request: () => ({ gender: this.gender(), page: this.paginationService.currentPage() - 1 }),
     loader: ({ request }) => {
       return this.productsService.getProducts({
-        gender: request.gender,
+        gender: request.gender, offset: request.page * 9
       });
     },
   });
--- 09-tesloShop-app/src/app/store-front/pages/home-page/home-page.component.ts
+++ 09-tesloShop-app/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,10 +1,9 @@
-import { ActivatedRoute } from '@angular/router';
-import { Component, inject, resource } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '@products//components/product-card/product-card.component';
 import { ProductService } from '@products//services/Product.service';
 import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
-import { map } from 'rxjs';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
@@ -14,17 +13,10 @@ import { map } from 'rxjs';
 export class HomePageComponent {
 
   private productsService = inject(ProductService);
-  private activatedRoute =  inject(ActivatedRoute);
-
-  currentPage = toSignal( this.activatedRoute.queryParamMap.pipe(
-    map( params => (params.get('page') ? +params.get('page')! : 1)),
-    map( page => (isNaN(page) ? 1 : page))
-  ), {
-    initialValue: 1
-  })
+  paginationService = inject(PaginationService);
 
   productsResource = rxResource( {
-    request: () => ({page: this.currentPage() - 1}),
+    request: () => ({page: this.paginationService.currentPage() - 1}),
     loader: ({request}) => {
       return this.productsService.getProducts({
         offset: request.page * 9
