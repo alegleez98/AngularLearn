de1a5fb8557c3eeed1b2903070a46694f7700ae7
Alejandro González González <agongon@arte-consultores.com>
2025-04-20 13:00:55 +0100

    Caché de productos

--- 09-tesloShop-app/src/app/products/services/Product.service.ts
+++ 09-tesloShop-app/src/app/products/services/Product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { Product, ProductsResponse } from '../interfaces/product.interface';
 import { environment } from 'src/environments/environment';
 
@@ -17,19 +17,25 @@ interface Options {
 })
 export class ProductService {
 
-
+  private productsCache = new Map<String, ProductsResponse>();
   private http = inject(HttpClient);
 
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 9, gender = '' } = options;
-    console.log(limit);
+    const key = `${limit}-${offset}-${gender}`;
+    if ( this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!);
+    }
     return this.http.get<ProductsResponse>(`${BASE_URL}/products`, {
       params: {
         limit: limit,
         offset: offset,
         gender: gender,
       }
-    }).pipe(tap((resp) => console.log(resp)));
+    }).pipe(
+      tap((resp) => console.log(resp)),
+      tap((resp) => this.productsCache.set(key, resp))
+    );
   }
 
   getProductsByIdSlug(idSlug: string): Observable<Product> {
