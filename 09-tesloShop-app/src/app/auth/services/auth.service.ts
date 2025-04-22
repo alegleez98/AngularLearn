78a6882ea8766b67e88c23d76b0ce909cf8fe5fd
Alejandro González González <agongon@arte-consultores.com>
2025-04-22 18:14:03 +0100

    Interceptores en Angular

--- 09-tesloShop-app/src/app/auth/services/auth.service.ts
+++ 09-tesloShop-app/src/app/auth/services/auth.service.ts
@@ -57,9 +57,9 @@ export class AuthService {
     }
 
     return this.http.get<AuthResponse>(`${baseUrl}/auth/check-status`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      }
+      // headers: {
+      //   Authorization: `Bearer ${token}`,
+      // }
     }).pipe(
       map(resp => this.handleAuthSuccess(resp)),
       catchError((error: any) => this.handleAuthError(error))
@@ -73,7 +73,7 @@ export class AuthService {
     this._token.set(null);
     this._authStatus.set('not-authenticated');
 
-    localStorage.removeItem('token');
+    //localStorage.removeItem('token');
   }
 
   private handleAuthSuccess( resp: AuthResponse) {
---
+++ 09-tesloShop-app/src/app/shared/interceptors/logging.interceptor.ts
@@ -0,0 +1,12 @@
+import { HttpEventType, type HttpHandlerFn, type HttpInterceptorFn, type HttpRequest } from '@angular/common/http';
+import { tap } from 'rxjs';
+
+export const loggingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+  return next(req).pipe(
+    tap((event) => {
+      if (event.type === HttpEventType.Response) {
+        console.log(req.url, 'returned a response with status', event.status);
+      }
+    })
+  );
+};
