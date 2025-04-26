45dd9c81c450bb14787d7fbac4b2efef9ac768f7
Alejandro González González <agongon@arte-consultores.com>
2025-04-26 09:33:29 +0100

    Listado de productos

---
+++ 09-tesloShop-app/src/app/products/components/product-table/product-table.component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+import { Product } from '@products//interfaces/product.interface';
+import { ProductImagePipe } from "../../pipes/productImage.pipe";
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'product-table',
+  imports: [ProductImagePipe, RouterLink, CurrencyPipe],
+  templateUrl: './product-table.component.html',
+})
+export class ProductTableComponent {
+
+  products = input.required<Product[]>();
+
+
+}
